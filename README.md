94089cbcc55b8d395223eb49d7fca085432b9fa9
Mike Turner <turner.mike@gmail.com>
2020-06-22 11:25:57 -0400

    section 3 complete

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Typescript: The Complete Developer's Guide [2020]
 
 [https://www.udemy.com/course/typescript-the-complete-developers-guide](https://www.udemy.com/course/typescript-the-complete-developers-guide)
+
+## Run
+
+Using TS Node to run Typescript with node.
+`ts-node index.ts`
--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -33,5 +33,26 @@ const logNumber: (i: number) => void = (i: number) => {
 // When to use annotations
 // 1) Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
-const coordinates = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // or use inferance and use "let foundWord = fasle;"
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
